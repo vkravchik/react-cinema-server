a10a9acce1063a160a01432bc1f2a8efdfa4d5c8
vkravc <vkravchik@gmail.com>
2020-01-14 11:05:53 +0200

    change port

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
@@ -8,9 +6,6 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const cfg = require('./cfg/db');
-
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -23,16 +18,13 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
 MongoClient.connect(cfg.url, (err, database) => {
   if (err) console.log(err);
   const db = database.db(cfg.dbName);
 
   require('./routes/users')(app, db);
 
-  app.listen(process.env.PORT || 30002, () => {
+  app.listen(process.env.PORT || 3002, () => {
     console.log('Server started on port ' + 3002);
   });
 });
